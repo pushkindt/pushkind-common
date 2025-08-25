291e159469f1e3504b2f8ebbc5dc3558dac710ef
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-08-25 15:06:37 +0300

    Restored original send_zmq_message

--- src/zmq.rs
+++ src/zmq.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use std::{thread, time::Duration};
 
-/// Serialize `msg` and send it to `zmq_address` using a ZMQ `PUB` socket.
+/// Serialize `msg` and send it to `zmq_address` using a ZMQ `PUSH` socket.
 ///
 /// A new socket is created for each call. Any serialization or socket errors
 /// are bubbled up to the caller.
@@ -10,11 +10,33 @@ pub fn send_zmq_message<T: Serialize>(
     zmq_address: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let context = zmq::Context::new();
-    let requester = context.socket(zmq::PUB)?;
+    let requester = context.socket(zmq::PUSH)?;
     requester.connect(zmq_address)?;
 
+    let serialized = serde_json::to_vec(msg)?;
+    requester.send(&serialized, 0)?;
+
+    log::info!("Sent {} bytes to {}", serialized.len(), zmq_address);
+
+    Ok(())
+}
+
+/// Serialize `msg` and send it to `zmq_address` using a ZMQ `PUB` socket.
+///
+/// A new socket is created for each call. Any serialization or socket errors
+/// are bubbled up to the caller.
+pub fn send_zmq_message_pub<T: Serialize>(
+    msg: &T,
+    zmq_address: &str,
+    delay: Option<u64>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let context = zmq::Context::new();
+    let requester = context.socket(zmq::PUB)?;
+    requester.connect(zmq_address)?;
     // Give the proxy/subscribers a moment to connect & propagate subscriptions
-    thread::sleep(Duration::from_millis(200));
+    if let Some(delay) = delay {
+        thread::sleep(Duration::from_millis(delay));
+    }
 
     let serialized = serde_json::to_vec(msg)?;
     requester.send(&serialized, 0)?;
