240505022c96433853482417c945f49be7e532dd
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-09-05 16:40:36 +0300

    Support relative auth redirect URLs

--- tests/middleware.rs
+++ tests/middleware.rs
@@ -33,6 +33,31 @@ async fn redirects_unauthorized_to_signin() {
 }
 
 #[actix_web::test]
+async fn redirects_unauthorized_to_relative_signin() {
+    let server_config = CommonServerConfig {
+        secret: "secret".to_string(),
+        auth_service_url: "/auth/signin".to_string(),
+    };
+
+    let app = test::init_service(
+        App::new()
+            .wrap(RedirectUnauthorized)
+            .app_data(web::Data::new(server_config.clone()))
+            .default_service(web::to(|| async { HttpResponse::Unauthorized().finish() })),
+    )
+    .await;
+
+    let req = test::TestRequest::default().to_request();
+    let resp = test::call_service(&app, req).await;
+
+    assert_eq!(resp.status(), StatusCode::SEE_OTHER);
+    assert_eq!(
+        resp.headers().get(header::LOCATION).unwrap(),
+        "/auth/signin?next=http%3A%2F%2Flocalhost%3A8080%2F"
+    );
+}
+
+#[actix_web::test]
 async fn success_response_passes_through() {
     let server_config = CommonServerConfig {
         secret: "secret".to_string(),
